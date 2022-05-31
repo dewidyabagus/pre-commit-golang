b2aa2e00cd35013e71856fb1c00b57bb3138157e
widyaadeb <widya.ade@majoo.id>
2022-05-31 14:52:36 +0700

    remove loopclosure

---
+++ business/tvet_main/vet-main.go
@@ -0,0 +1,23 @@
+package tvet_main
+
+// import "sync"
+
+// func AssignmentLock() {
+// 	var lock sync.Mutex
+
+// 	l := lock
+// 	l.Lock()
+// 	defer l.Unlock()
+// }
+
+// func LoopClosure() {
+// 	var wg sync.WaitGroup
+// 	for _, v := range []int{0, 1, 2, 3} {
+// 		wg.Add(1)
+// 		go func() {
+// 			fmt.Println(v)
+// 			wg.Done()
+// 		}()
+// 	}
+// 	wg.Wait()
+// }
